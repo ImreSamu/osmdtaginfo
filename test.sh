7dde792d3832cc0a1698fe4ae5cbd7533213148e
ImreSamu <ImreSamu@users.noreply.github.com>
2016-11-02 22:07:31 +0100

    modify for travis test ..

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ rm -f ./import/*
 wget -q -N http://download.geofabrik.de/central-america-latest.osm.pbf  -P ./import
 ls -la ./import/*
 make build
+
 echo " Start test  ni - Nicaragua "
 docker-compose run --rm taginfo_job_ni /osm/sh/osm_split.sh
 docker-compose run --rm taginfo_job_ni /osm/sh/osm_jobinit.sh
