8e622a8bc6484ec7547f605d38645a9be781b95b
ImreSamu <ImreSamu@users.noreply.github.com>
2017-02-18 17:23:56 +0100

    set central-america + Nicaragua as a testing process

--- test.sh
+++ test.sh
@@ -5,7 +5,18 @@ wget -q -N http://download.geofabrik.de/central-america-latest.osm.pbf  -P ./imp
 ls -la ./import/*
 make build
 
-echo " Start test  ni - Nicaragua "
-docker-compose run --rm taginfo_job_ni /osm/sh/osm_split.sh
-docker-compose run --rm taginfo_job_ni /osm/sh/osm_jobinit.sh
+docker-compose run -e CONTINENT_LONG=central-america   -e CONTINENT=ca  -e START_PORT=6800 taginfo_dev  /osm/setup/r.sh
 
+CONTINENT=ca
+cd ./service/$CONTINENT
+
+### ./service_create.sh
+
+ docker-compose create taginfo_view_ca_ni
+
+### ./job.sh
+
+ echo " Start processing ca_ni - Nicaragua "
+ time docker-compose run --rm taginfo_job_ca_ni /osm/sh/osm_split.sh
+ time docker-compose run --rm taginfo_job_ca_ni /osm/sh/osm_jobinit.sh
+ time docker-compose restart taginfo_view_ca_ni
